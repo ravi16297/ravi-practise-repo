53ecba179fe2b0294410738190d5d4d768befd1e
ravi16297 <ravikumar16297@gmail.com>
2023-03-03 00:27:06 +0530

    commit two file

---
+++ NEW.PY.py
@@ -0,0 +1,3 @@
+b=23
+c=23**4
+print("print the cube of number -",c)
--- substraction.py
+++ substraction.py
@@ -3,3 +3,4 @@ b=30
 c=b-a
 print("print the value of c-",c)
 print("vishal is tension free person")
+print("jayant baba ki jai")
